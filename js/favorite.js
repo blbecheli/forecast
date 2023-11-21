f84c27bfa706f179a8adfe0ddba8db985a2966b8
blbecheli <blbecheli@gmail.com>
2023-11-21 14:36:58 -0800

    fixed favorite

--- js/favorite.js
+++ js/favorite.js
@@ -2,176 +2,63 @@ const favoriteStar = document.getElementById('star');
 const cityInput = document.querySelector("#city-input");
 const cityLocal = document.querySelector("#cityLocal");
 
+// Adicione as classes iniciais ao carregar a página, verificando o localStorage
+window.addEventListener('load', () => {
+    updateFavoriteStar();
+});
+
 export const favorite = () => {
     favoriteStar.addEventListener('click', (e) => {
-        e.preventDefault();        
+        e.preventDefault();
         const google = cityInput.value.trim();
-        const favorite = cityLocal.value        
-        saveFav(google, favorite)
-    })
-}
+        const favorite = cityLocal.value.trim();
+        saveFav(google, favorite);
+        updateFavoriteStar();
+    });
+};
 
 function saveFav(google, favorite) {
-    const cityGoogle = cityInput.value.split(',')[0].trim();
-    const cityStg = cityLocal.value.split(',')[0].trim();
-    const cityTitle = document.querySelector('#city').textContent;    
+    const cityGoogle = google.split(',')[0].trim();
+    const cityStg = favorite.split(',')[0].trim();
 
-    let cityStorage = [];
-    if (localStorage.getItem('cityStorage')) {
-        cityStorage = JSON.parse(localStorage.getItem('cityStorage'));
+    let cityStorage = JSON.parse(localStorage.getItem('cityStorage')) || [];
+
+    const handleCity = (city) => {
+        const isCitySaved = cityStorage.some(item => item.city === city);
+        const existingCityIndex = cityStorage.findIndex(item => item.city === city);
+
+        if (!isCitySaved) {
+            cityStorage.push({ city });
+            favoriteStar.classList.add('fa-solid');
+            favoriteStar.classList.remove('fa-regular');
+        } else {
+            cityStorage.splice(existingCityIndex, 1);
+            favoriteStar.classList.add('fa-regular');
+            favoriteStar.classList.remove('fa-solid');
+        }
+
+        localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
+    };
+
+    if (cityGoogle) {
+        handleCity(cityGoogle);
     }
 
-    if (cityTitle === cityGoogle && google !== '') {
-        const handleGoogle = (city) => {
-            const isCitySaved = cityStorage.some(item => item.city === city);
-            const existingCityIndex = cityStorage.findIndex(item => item.city === city);
-            if (!isCitySaved) {
-                cityStorage.push({ city });
-                localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-                favoriteStar.classList.add('fa-solid');
-                favoriteStar.classList.remove('fa-regular');                
-            } else {
-                cityStorage.splice(existingCityIndex, 1);
-                localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-                favoriteStar.classList.add('fa-regular');
-                favoriteStar.classList.remove('fa-solid');                
-            }
-        };
-
-        handleGoogle(google);
-    }
-
-    if (cityTitle === cityStg && favorite !== '') {
-        const handleFavorite = (favorite) => {
-            const isCitySaved = cityStorage.some(item => item.city === favorite);
-            const existingCityIndex = cityStorage.findIndex(item => item.city === favorite);
-            if (!isCitySaved) {
-                cityStorage.push({ city: favorite });
-                localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-                favoriteStar.classList.add('fa-solid');
-                favoriteStar.classList.remove('fa-regular');
-                console.log('Cidade salva:', favorite);
-            } else {
-                cityStorage.splice(existingCityIndex, 1);
-                localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-                favoriteStar.classList.add('fa-regular');
-                favoriteStar.classList.remove('fa-solid');
-                console.log('Cidade removida:', favorite);
-            }
-        };
-
-        handleFavorite(favorite);
+    if (cityStg) {
+        handleCity(cityStg);
     }
 }
 
+function updateFavoriteStar() {
+    const cityTitle = document.querySelector('#city').textContent;
+    const cityStorage = JSON.parse(localStorage.getItem('cityStorage')) || [];
 
-
-
-
-// function local(cityInputValue, cityLocal) {
-//     console.log(cityInputValue, cityLocal);
-//     let cityStorage = [];
-//     if (localStorage.getItem('cityStorage')) {
-//         cityStorage = JSON.parse(localStorage.getItem('cityStorage'));
-//     }
-//     // Verificar se o dado já está salvo
-//     const isCitySaved = cityStorage.some(item => item.city === cityInputValue);
-//     const existingCityIndex = cityStorage.findIndex(item => item.city === cityInputValue);
-//     const isCitySavedFav = cityStorage.some(item => item.city === cityLocal);
-//     const existingCityIndexFav = cityStorage.findIndex(item => item.city === cityLocal);
-
-//     // if(existingCityIndex !==-1){
-//     //     favoriteStar.classList.add('fa-solid');
-//     //     favoriteStar.classList.remove('fa-regular');
-//     // } Lugar errado
-
-//     if (!isCitySaved) {
-//         cityStorage.push({ city: cityInputValue });
-//         localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-//         favoriteStar.classList.add('fa-solid');
-//         favoriteStar.classList.remove('fa-regular');
-//         console.log('Cidade salva:', cityInputValue);
-//     };
-//     if (existingCityIndex !== -1) {
-//         cityStorage.splice(existingCityIndex, 1);
-//         localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-//         favoriteStar.classList.add('fa-regular');
-//         favoriteStar.classList.remove('fa-solid');
-//         console.log('Cidade removida:', cityInputValue);
-//     };
-//     if (!isCitySavedFav) {
-//         cityStorage.push({ city: cityLocal });
-//         localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-//         favoriteStar.classList.add('fa-solid');
-//         favoriteStar.classList.remove('fa-regular');
-//         console.log('Cidade salva:', cityLocal);
-//     };
-//     if (existingCityIndex !== -1) {
-//         cityStorage.splice(existingCityIndex, 1);
-//         localStorage.setItem('cityStorage', JSON.stringify(cityStorage));
-//         favoriteStar.classList.add('fa-regular');
-//         favoriteStar.classList.remove('fa-solid');
-//         console.log('Cidade removida:', cityLocal);
-//     };
-// }
-
-
-
-// //Save date in Local Storage
-// // function local() {
-// //     let cityStorage = []
-// //     if (localStorage.hasOwnProperty('cityStorage')) {
-// //         cityStorage = JSON.parse(localStorage.getItem('cityStorage'))
-// //     }
-// //     cityStorage.push({ city: cityInput.value })
-// //     localStorage.setItem("cityStorage", JSON.stringify(cityStorage))
-// // }
-
-// // export function modifyCityStorage(city) {
-// //     const cityInput = document.querySelector("#city-input");
-// //     let cityStorage = [];
-
-// //     if (localStorage.hasOwnProperty('cityStorage')) {
-// //         cityStorage = JSON.parse(localStorage.getItem('cityStorage'));
-// //     }
-
-// //     const inputValue = cityInput.value;
-
-// //     const indexToRemove = cityStorage.findIndex(item => item.city === city);
-// //     if (indexToRemove !== -1) {
-// //         // Se o valor existe, remova o item da matriz
-// //         cityStorage.splice(indexToRemove, 1);
-// //     } else {
-// //         // Se o valor não existe, adicione-o à matriz
-// //         cityStorage.push({ city: city });
-// //     }
-
-// //     localStorage.setItem("cityStorage", JSON.stringify(cityStorage));
-// // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function showLocal() {
-
-//     const dateSave = localStorage.getItem('cityStorage');
-//     if (dateSave) {
-//         var dadosConvertidos = JSON.parse(dateSave);
-//         dadosConvertidos.forEach(function (item) {
-//             var option = document.createElement('option');
-//             option.value = item.city;
-//             option.textContent = item.city;
-//             lista.appendChild(option);
-//         });
-//     }
-// }
+    const isCitySaved = cityStorage.some(item => item.city === cityTitle);
+    if (isCitySaved) {
+        favoriteStar.classList.add('fa-solid');
+        favoriteStar.classList.remove('fa-regular');
+    } else {
+        favoriteStar.classList.add('fa-regular');
+        favoriteStar.classList.remove('fa-solid');
+    }
+}
